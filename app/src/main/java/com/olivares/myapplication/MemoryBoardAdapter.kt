472eeaaba7a1344b87de8a6ffbbaaee24accd232
Mario Olivares <MarioOlivares_95@hotmail.com>
2022-06-23 12:44:46 -0400

    Added 4x2 hard coded. Need to add functionality to chose different sizes

--- app/src/main/java/com/olivares/myapplication/MemoryBoardAdapter.kt
+++ app/src/main/java/com/olivares/myapplication/MemoryBoardAdapter.kt
@@ -1,18 +1,40 @@
 package com.olivares.myapplication
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        // Set size of each element here
+        val cardWidth = (parent.width / 2) - (2 * MARGIN_SIZE)
+        val cardHeight = (parent.height / 4) - (2 * MARGIN_SIZE)
+        val cardLength = min(cardHeight, cardWidth)
+
         // Inflate our layout here
-        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+
+        // Set the width and height here
+        val layoutParams =
+            view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardLength
+        layoutParams.height = cardLength
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
         return ViewHolder(view)
     }
 
@@ -25,10 +47,15 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
 
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
         fun bind(position: Int) {
-            // No-op
+            imageButton.setOnClickListener {
+                Log.i(TAG, "Clicked on position $position ")
+
+            }
 
         }
-    }
 
+    }
 }
